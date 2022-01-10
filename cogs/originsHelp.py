b0c4d9f63fa6df49b531cd863656b6e6afe602c4
KarsonTheFoxx <95767655+KarsonTheFoxx@users.noreply.github.com>
2022-01-09 18:29:49 -0700

    Add files via upload

---
+++ cogs/originsHelp.py
@@ -0,0 +1,71 @@
+from discord.ext import commands
+import discord
+import json as js
+from os import listdir
+
+def check(author):
+  def inner(message):
+    return message.author == author
+  return inner
+
+def get_color(origin, colors):
+  color = colors[origin["color"]]
+  return color
+
+class OriginNotFound(Exception):
+  pass
+
+#colors
+colors = {
+  "red": discord.Color.red(),
+  "blue": discord.Color.blue(),
+  "grey": discord.Color.greyple(),
+  "orange": discord.Color.orange(),
+  "white": 0xeeffee,
+  "blue": discord.Color.blue(),
+  "dark_grey": discord.Color.dark_grey(),
+  "dark_orange": discord.Color.dark_orange(),
+  "dark_blue": discord.Color.dark_blue(),
+  "black": discord.Color.from_rgb(0, 0, 0),
+  "dark_purple": discord.Color.dark_purple(),
+  "purple": discord.Color.purple()
+}
+#setup cog
+class origins_infoCog(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    self.db = "./db/"
+  
+  @commands.command(name="origin")
+  async def origins_help(self, ctx, origin):
+    try:
+      with open(self.db+"origins.json", "r") as origins:
+        origins = js.load(origins)
+      
+      found = False
+
+      for Origin in origins["origins"]:
+        if Origin["name"] == origin.lower():
+          found = True
+          emb = discord.Embed(title=origin.capitalize(), description=Origin["description"], color=get_color(Origin, colors))
+          origin_icon = discord.File(Origin["icon"], filename="originicon.png")
+          emb.set_thumbnail(url="attachment://originicon.png")
+
+          for power in Origin["powers"]:
+            emb.add_field(name=power["name"], value=power["description"], inline=True)
+          
+          await ctx.send(file=origin_icon, embed=emb)
+        
+      if found == False:
+        raise OriginNotFound
+
+    except FileNotFoundError:
+      emb = discord.Embed(title="the database seems to be missplaced", description="i cannot find the origins.json")
+      await ctx.send(embed=emb)
+    except OriginNotFound:
+      emb = discord.Embed(title="origin not found", description="check the spelling of the origin and try again", color=discord.Color.orange())
+      emb.set_footer(text="The selected origin may not have been added to my database yet")
+      await ctx.send(embed=emb)
+
+def setup(bot):
+  bot.add_cog(origins_infoCog(bot))
